a63274e3ce479a7f699595552824d98ecb2b44f5
diwakarmalla <mdiwakar50@gmail.com>
2025-02-19 13:41:18 -0600

    Updated prometheus spec for pod monitors

--- roles.tf
+++ roles.tf
@@ -70,7 +70,7 @@ resource "aws_iam_role" "dev_role" {
       {
         Effect    = "Allow",
         Principal = {
-          AWS = "arn:aws:iam::851725392626:root"
+          AWS = "arn:aws:iam::211125344879:root"
         },
         Action    = "sts:AssumeRole",
         Condition = {}
